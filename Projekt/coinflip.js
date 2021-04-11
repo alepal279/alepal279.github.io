46d63cbfe3bbe354e0fac3639b86254da8de7fb6
alepal279 <70194223+alepal279@users.noreply.github.com>
2021-04-11 19:01:57 +0200

    Add files via upload

--- Projekt/coinflip.js
+++ Projekt/coinflip.js
@@ -1,6 +1,10 @@
 
 var points = 100;
+var wins = 0;
+var losses = 0;
 document.getElementById("points").innerHTML="points: "+points;
+document.getElementById("wins").innerHTML = "Wins: "+wins;
+document.getElementById("losses").innerHTML = "Losses: "+losses;
 
 
 function tossHeads(){
@@ -8,14 +12,22 @@ var image = new Array("Bilder/heads.png","Bilder/tails.png");
 var randomImage = Math.floor(Math.random()*image.length);
 document.getElementById("coin").src = image[randomImage];
 if(randomImage==0){
-    document.getElementById("res").innerHTML = "You Win";
+    document.getElementById("res").innerHTML = "You Win 10 points";
     points+=10;
     document.getElementById("points").innerHTML="points "+points;
+    wins++;
+    document.getElementById("wins").innerHTML = "Wins: "+wins;
 }
 else{
-    document.getElementById("res").innerHTML = "You Loose";
+    document.getElementById("res").innerHTML = "You Loose 10 points";
     points-=10;
     document.getElementById("points").innerHTML="points: "+points;
+    losses++;
+    document.getElementById("losses").innerHTML = "Losses: "+losses;
+}
+if(points<=0){
+    alert("You are out of points");
+    location.reload();
 }
 }
 
@@ -24,13 +36,23 @@ function tossTails(){
     var randomImage = Math.floor(Math.random()*image.length);
     document.getElementById("coin").src = image[randomImage];
     if(randomImage==0){
-        document.getElementById("res").innerHTML = "You Loose";
+        document.getElementById("res").innerHTML = "You Loose 10 points";
         points-=10;
         document.getElementById("points").innerHTML="points: "+points;
+        losses++;
+        document.getElementById("losses").innerHTML = "Losses: "+losses;
     }
     else{
-        document.getElementById("res").innerHTML = "You Win";
+        document.getElementById("res").innerHTML = "You Win 10 points";
         points+=10;
         document.getElementById("points").innerHTML="points: "+points;
+        wins++;
+        document.getElementById("wins").innerHTML = "Wins: "+wins;
+    }
+    if(points<=0){
+        alert("You are out of points");
+        location.reload();
     }
     }
+
+
--- Projekt/form.js
+++ Projekt/form.js
@@ -2,5 +2,10 @@ function myFunction() {
     var age = document.getElementById("age").value;
     var lname = document.getElementById("lname").value;
     var fname = document.getElementById("fname").value;
-        document.getElementById("demo").innerHTML = "<p>Mitt namn är " + fname + " " + lname + " och jag är " + age + " år gammal</p>";
+    if(age>17){
+        document.getElementById("demo").innerHTML = "<p>Thank you " + fname + " " + lname + "! Your information has been noted.</p>";
+    }
+    else{
+        document.getElementById("demo").innerHTML = "<p>We are sorry to tell you that you are a bit too young to work for us right now.</p>"
+    }
     }
